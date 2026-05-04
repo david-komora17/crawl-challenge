e69a6ddfa4cf2329711a9b0a4c6d9024b057e3cb
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-05-04 20:57:16 +0300

    Create request headers indicating that the request is made by a browser and add the try except block for error handling when sourcing data.

--- challenge.py
+++ challenge.py
@@ -3,4 +3,19 @@ from bs4 import BeautifulSoup
 
 def scrape_bbc_news():
     url = "https://www.bbc.com/news"
+
+    # Fetch the content
+    # Add a user-agent string to tell the website we are a browser and not a script.
+    headers = {
+        "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; win64; x64;) AppleWebKit/527.36 (KHTML) Chrome/119.0.0.0 Safari/537.36"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching page: {e}")
+        return
     
+    soup = BeautifulSoup(response.text, 'html.parser')
